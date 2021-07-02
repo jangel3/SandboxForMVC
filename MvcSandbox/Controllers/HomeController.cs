dd82d18757fbeb5700f7858fcfbbe3ddf9c9d48d
John Angel <john.angel@insperity.com[http]>
2021-07-02 10:41:55 -0500

    Initial commit.

---
+++ MvcSandbox/Controllers/HomeController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using MvcSandbox.Models;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcSandbox.Controllers
+{
+    [TypeFilter(typeof(OutageAuthorizationFilter))]
+    public class HomeController : Controller
+    {
+        //private readonly ILogger<HomeController> _logger;
+
+        //public HomeController(ILogger<HomeController> logger)
+        //{
+        //    _logger = logger;
+        //}
+
+        [ModelBinder]
+        public string Id { get; set; }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [Route("/contact-us", Name = "Contact")]
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MvcSandbox/Middleware/FeatureSwitchMiddleware.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+
+namespace MvcSandbox.Middleware
+{
+    public class FeatureSwitchMiddleware
+    {
+        private readonly RequestDelegate _next;
+
+        public FeatureSwitchMiddleware(RequestDelegate next)
+        {
+            _next = next;
+        }
+
+        public async Task Invoke(HttpContext httpContext, IConfiguration config)
+        {
+            if (httpContext.Request.Path.Value.Contains("/features"))
+            {
+                var switches = config.GetSection("FeatureSwitches");
+
+                var report = switches.GetChildren().Select(x => $"{x.Key} : {x.Value}");
+
+                await httpContext.Response.WriteAsync(string.Join("\n", report));
+            }
+            else
+            {
+                await _next(httpContext);
+            }
+        }
+    }
+}
